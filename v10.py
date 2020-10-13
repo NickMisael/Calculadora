2fc5f67c11fe3258993da8c14150e15df7e8f04a
NickMisael <40251538+NickMisael@users.noreply.github.com>
2020-10-13 13:16:52 -0700

    Update v10.py

--- v10.py
+++ v10.py
@@ -9,8 +9,14 @@ def limpaLabel(string, tam):
     while i < tam:
         nstring += letter[i] + " "
         i += 1
-    return nstring         
-    
+    return nstring        
+
+
+def LimpaCaixa():
+    ResultBox['text'] = limpaLabel(ResultBox['text'],lenR)
+
+def FechaJanela():
+    janela.destroy()
 
 def PressButton():
     ResultBox['text'] = limpaLabel(ResultBox['text'],lenR)
@@ -41,61 +47,73 @@ def PressButton():
 janela = Tk()
 janela.title('Calculadora v1.1')
 
-TextBox1 = Entry(janela, background = 'gray' ,width = 10, font = 'Arial18')
-TextBox1.place(x=210,y=100)
-TextBox2 = Entry(janela, background = 'gray' ,width = 10, font = 'Arial18')
-TextBox2.place(x=337,y=100)
+TextBox1 = Entry(janela, background = 'gray' ,width = 16, font = 'Arial18', bg = 'white')
+TextBox1.place(x=18,y=65)
+TextBox2 = Entry(janela, background = 'gray' ,width = 16, font = 'Arial18', bg = 'white')
+TextBox2.place(x=192,y=65)
 
 status1 = IntVar()
-label1 = Label(janela, text='+', font = 'Arial 12')
-label1.place(x=318,y=100)
-ResultBox = Label(janela, text='A soma eh: ', font = 'Arial 12') 
-ResultBox.place(x=212,y=150)
+label1 = Label(janela, text='+', font = 'Arial 12', bg = '#4B0082', fg = 'white')
+label1.place(x=173,y=66)
+ResultBox = Label(janela, text='A soma eh: ', font = 'Arial 12', bg = '#4B0082', fg = 'white') 
+ResultBox.place(x=22,y=125)
 splR  = ResultBox['text'].split()
 lenR = len(splR)
 def labSum():
     label1['text'] = '+'
+    label1['font'] = 'Arial 12'
     ResultBox['text'] = 'A soma eh: '
     splR  = ResultBox['text'].split()
     lenR = len(splR)
 
 def labDif():
     label1['text'] = '-'
+    label1['font'] = 'Arial 18'
     ResultBox['text'] = 'A diferenca eh: '
     splR  = ResultBox['text'].split()
     lenR = len(splR)
 
 def labProd():
     label1['text'] = '*'
+    label1['font'] = 'Arial 15'
     ResultBox['text'] = 'O produto eh: '
     splR  = ResultBox['text'].split()
     lenR = len(splR)
 
 def labQuo():
     label1['text'] = '/'
+    label1['font'] = 'Arial 15'
     ResultBox['text'] = 'O quociente eh: '
     splR  = ResultBox['text'].split()
     lenR = len(splR)
 
-CheckBox1 = Checkbutton(janela, text='Adicao', variable=status1, onvalue=1, offvalue=0, command = labSum)
+CheckBox1 = Checkbutton(janela, text='Adicao', variable=status1, onvalue=1, offvalue=0, selectcolor = '#008080' , bg = '#4B0082', fg = 'white', command = labSum)
 CheckBox1.select()
 CheckBox1.pack()
-CheckBox1.place(x=210,y=75)
+CheckBox1.place(x=25,y=25)
 
-CheckBox2 = Checkbutton(janela, text='Subtracao', variable=status1, onvalue=3, offvalue=2, command = labDif)
+CheckBox2 = Checkbutton(janela, text='Subtracao', variable=status1, onvalue=3, offvalue=2, selectcolor = '#008080' , bg = '#4B0082', fg = 'white', command = labDif)
 CheckBox2.pack()
-CheckBox2.place(x=275,y=75)
+CheckBox2.place(x=90,y=25)
 
-CheckBox3 = Checkbutton(janela, text='Multiplicacao', variable=status1, onvalue=5, offvalue=4, command = labProd)
+CheckBox3 = Checkbutton(janela, text='Multiplicacao', variable=status1, onvalue=5, offvalue=4, selectcolor = '#008080' , bg = '#4B0082', fg = 'white', command = labProd)
 CheckBox3.pack()
-CheckBox3.place(x=355,y=75)
+CheckBox3.place(x=173,y=25)
 
-CheckBox4 = Checkbutton(janela, text='Divisao', variable=status1, onvalue=7, offvalue=6, command = labQuo)
+CheckBox4 = Checkbutton(janela, text='Divisao', variable=status1, onvalue=7, offvalue=6, selectcolor = '#008080' , bg = '#4B0082', fg = 'white', command = labQuo)
 CheckBox4.pack()
-CheckBox4.place(x=450,y=75)
+CheckBox4.place(x=273,y=25)
 
-btn = Button(janela, width = 25, text = 'Calcular', command = PressButton, background = 'blue')
-btn.place(x=205,y=175)
+btn = Button(janela, width = 25, text = 'Calcular', command = PressButton, background = 'blue', fg = 'white')
+btn.place(x=75,y=165)
 
-janela.geometry('640x480+250+100')
+btnLimpa = Button(janela, width = 15, text = 'Limpar', command = LimpaCaixa, background = 'green', fg = 'white')
+btnLimpa.place(x=10,y=225)
+
+btnFecha = Button(janela, width = 15, text = 'Fechar', command = FechaJanela, background = 'red', fg = 'white')
+btnFecha.place(x=215,y=225)
+
+janela.geometry('360x280+250+100')
+janela.configure(bg='#4B0082')
+janela.resizable(0,0)
 janela.mainloop()
